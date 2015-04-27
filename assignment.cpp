223c25f60d6a52dfc5ef00eee3baa8586b83702d
Amirhossein <amirhnaimi@gmail.com>
2015-04-26 21:03:26 -0500

    Added insert, search and print functions

--- assignment.cpp
+++ assignment.cpp
@@ -8,12 +8,15 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <list>
 
 using namespace std;
 
 class btree
 {
 private:
+
+    int nnodes;
 
     struct node
     {
@@ -37,6 +40,8 @@ public:
 
     bool search(int);
 
+    void printLevels();
+
 private:
 
     void inorder(node*);
@@ -49,6 +54,8 @@ private:
 
 btree::btree() {
     // set the root to NULL
+    root = NULL;
+    nnodes = 0;
 }
 
 bool btree::isEmpty()
@@ -56,22 +63,57 @@ bool btree::isEmpty()
     // This functions returns true if the tree is
     // empty and false if it is not empty. You just
     // need to look at the root.
+
+    return true;
 }
 
 void btree::insert(int d)
 {
     // this function must insert the value d in the tree
 
+    nnodes++;
+
+
+
     // first of all, check if d already exists in the tree
     // you can do this by calling to the search(int) function
 
+    if (search(d))
+        return;
+
     // if d is not in the tree already, create a new node with data equal d
 
+    node* n = new node;
+    n->data = d;
+    n->left = n->right = NULL;
+
     // if the tree is empty, set the root to the new node
+
+    if (root == NULL)
+    {
+        root = n;
+        return;
+    }
 
     // if the tree is not empty, look for the right place where to insert d
     // in order to do this, you may need to keep track of the potential
     // parent node to which the new node will be attached as a child
+
+    node *parent = NULL, *i = root;
+
+    while (i != NULL){
+        parent = i;
+
+        if (d < i->data)
+            i = i->left;
+        else
+            i = i->right;
+    }
+
+    if (d < parent->data)
+        parent->left = n;
+    else
+        parent->right = n;
 }
 
 void btree::remove(int d)
@@ -140,17 +182,75 @@ bool btree::search(int val)
 {
     // This function must call the private function
     // search_element(node*,int) passing the root and
-    // the integer value val as parameters. The function
-    // must use recursion.
+    // the integer value val as parameters and returning
+    // the same answer it gets from search_element()
+    // back to the main program.
+
+    return search_element(root, val);
 }
 
+/*
+// this function receives a node and an integer as
+// parameters and searches for the value val in the
+// data of the node. The function must be such that
+// if the value is never found, it returns false.
+// If the value is found, then it returns true.
+// The function must use recursion.
+*/
 bool btree::search_element(node* p, int val) {
-    // this function receives a node and an integer as
-    // parameters and searches for the value val in the
-    // data of the node. The function must be such that
-    // if the value is never found, it returns false.
-    // If the value is found, then it returns true.
-    // The function must use recursion.
+
+    if (p == NULL) {
+        return false;
+    }
+    else {
+        if (val == p->data) return(true);
+        else {
+            if (val < p->data)
+                return(search_element(p->left, val));
+            else
+                return(search_element(p->right, val));
+        }
+    }
+}
+
+/* Print by levels */
+void btree::printLevels() {
+
+    if (root == NULL)
+        return;
+
+    //printSides(root);
+    node *i;
+    int lev = 0;
+
+    std::list<node*> lstNode;
+    lstNode.push_back(root);
+    lstNode.push_back(NULL);
+
+    cout << endl << "Level 0 nodes: ";
+
+    while (!lstNode.empty()) {
+        i = lstNode.front();
+        lstNode.pop_front();
+
+        if (i == NULL)
+        {
+            if (!lstNode.empty()) {
+                lstNode.push_back(NULL);
+                cout << endl;
+                cout << "Level " << ++lev << " nodes: ";
+            }
+        }
+        else {
+            if (i->left != NULL)
+                lstNode.push_back(i->left);
+            if (i->right != NULL)
+                lstNode.push_back(i->right);
+
+            cout << i->data << " ";
+        }
+    }
+
 }
 
 int main(int argc, char* argv[])
@@ -178,20 +278,20 @@ int main(int argc, char* argv[])
     btree my_tree;
 
     // some auxiliary variables
-    int ch,tmp,tmp1;
+    int ch, tmp, tmp1;
     bool ans;
 
     // if arguments are passed, then the program assumes the
     // arguments are a list of integers and it inserts one by
     // one into the tree.
     if (argc > 1) {
-        for (int i=1; i < argc; i++) {
+        for (int i = 1; i < argc; i++) {
             tmp = atoi(argv[i]);
             my_tree.insert(tmp);
         }
     }
 
-    while(1)
+    while (1)
     {
         cout << endl << endl;
         cout << " Binary Tree Operations " << endl;
@@ -199,17 +299,18 @@ int main(int argc, char* argv[])
         cout << " 2. In-Order Traversal " << endl;
         cout << " 3. Pre-Order Traversal " << endl;
         cout << " 4. Post-Order Traversal " << endl;
-        cout << " 5. Removal "<< endl;
-        cout << " 6. Search "<< endl;
+        cout << " 5. Removal " << endl;
+        cout << " 6. Search " << endl;
+        cout << " 7. Print Tree by Levels" << endl;
         // ***************************************************
         // If you decide to implement the extra credit options
         // this is one place where you will need to add code
         // to provide the user with those extra functions
         // ***************************************************
-        cout << " 0. Exit "<< endl;
+        cout << " 0. Exit " << endl;
         cout << " Enter your choice : ";
         cin >> ch;
-        switch(ch)
+        switch (ch)
         {
         case 0:
             return 0;
@@ -244,12 +345,14 @@ int main(int argc, char* argv[])
             ans = my_tree.search(tmp1);
             if (ans) cout << tmp1 << " was found!!!" << endl;
             else cout << tmp1 << " was not found" << endl;
+        case 7:
+            my_tree.printLevels();
             break;
-        // ***************************************************
-        // If you decide to implement the extra credit options
-        // this is one place where you will need to add code
-        // to provide the user with those extra functions
-        // ***************************************************
+            // ***************************************************
+            // If you decide to implement the extra credit options
+            // this is one place where you will need to add code
+            // to provide the user with those extra functions
+            // ***************************************************
         }
     }
 }
